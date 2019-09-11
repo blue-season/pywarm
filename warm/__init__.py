5d88374e8b5ec54fba02280c15dde7a398b84b9b
blue-season <very.blue.season@gmail.com>
2019-09-10 22:38:17 -0700

    Fix docstring of warm.up

--- warm/__init__.py
+++ warm/__init__.py
@@ -1,3 +1,5 @@
 # 09-10-2019;
+
+""" `warm.up` is an alias of
+[`warm.engine.prepare_model_`](https://blue-season.github.io/pywarm/reference/warm/engine/#prepare_model_). """
 from warm.engine import prepare_model_ as up
-""" `warm.up` is an alias of `warm.engine.prepare_model_`. """
